e626e39f5431de7a9fd2190bf1eb5efb24306df2
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-15 03:41:15 -0500

    test again

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
-# Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
+# Create your models here. 
+class Reminder(models.Model):
+	username = models.CharField(max_length=128)
+	message = models.CharField(max_length=400)
+	time_created = models.DateTimeField('date created', auto_now_add=True)
+	time_left = models.DateTimeField('date created', auto_now_add=False)
