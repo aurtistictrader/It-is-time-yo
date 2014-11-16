ac122cd10716913172e3d5c53ee43e4ac23494c0
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-16 07:04:54 -0500

    fixed text input colour

--- hello/forms.py
+++ hello/forms.py
@@ -4,4 +4,3 @@ import datetime
 class NameForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'YO Username', 'size' : '30px', 'required' : 'required' }), label='', max_length=128)
     message = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'YO Message', 'size' : '30px', 'required' : 'required' }), label='', max_length=400)
-    time_left = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'When do I YO?', 'size' : '30px', 'required' : 'required' }), label='', max_length=20)
