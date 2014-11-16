07f90b5634e4892495385e54c5ca7b284c7bfbc8
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-16 07:21:18 -0500

    used django to develope dropdown

--- hello/forms.py
+++ hello/forms.py
@@ -4,3 +4,5 @@ import datetime
 class NameForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'YO Username', 'size' : '30px', 'required' : 'required' }), label='', max_length=128)
     message = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'YO Message', 'size' : '30px', 'required' : 'required' }), label='', max_length=400)
+    hours = forms.ChoiceField(choices=[(x, x) for x in range(0, 23)])
+    minutes = forms.ChoiceField(choices=[(x, x) for x in range(0, 59)])
