befe8ecd91c8ef099ea9449ec95d3ce49fb2df40
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-16 00:37:44 -0500

    testing time

--- hello/views.py
+++ hello/views.py
@@ -21,9 +21,12 @@ def submit(request):
             my_model = Reminder()
             my_model.username = form.cleaned_data.get('username')
             my_model.message = form.cleaned_data.get('message')
-            my_model.time_left = form.cleaned_data.get('time_left')
-            # my_model.time_left = strftime("%b %d %Y %H:%M:%S", gmtime())            #TODO: Change this depending on user input
-            my_model.time_created = strftime("%b %d %Y %H:%M:%S",gmtime())
+            my_model.date_created = strftime("%b %d %Y",gmtime())        
+            my_model.time_created = strftime("%H:%M:%S",gmtime())
+
+            my_model.date_left = strftime("%b %d %Y",gmtime())		#TODO: Change this depending on user input
+            my_model.time_left = #form.cleaned_data.get('time_left')
+            my_model.time_left = strftime("%H:%M:%S", gmtime())    
             my_model.save()	
             # process the data in form.cleaned_data as required
             # ...
