7f359491551b0823fd60bcd6a42479b8f229f97c
bfung <b6fung@uwaterloo.ca>
2014-11-16 23:51:52 -0800

    stufs

--- hello/models.py
+++ hello/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here. 
 class Reminder(models.Model):
 	username = models.CharField(max_length=128)
-	message = models.CharField(max_length=400)
 	date_created = models.DateTimeField('date created')
 	time_created = models.DateTimeField('time created')
 	date_left = models.DateTimeField('date reminded')
