4c54cca7afbaab01f570ec3feda7bfeae59fcbd6
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-16 19:11:24 -0500

    fxied minutes and hours

--- hello/forms.py
+++ hello/forms.py
@@ -4,5 +4,5 @@ import datetime
 class NameForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'YO Username', 'size' : '30px', 'required' : 'required' }), label='', max_length=128, required=True)
     message = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'YO Message', 'size' : '30px', 'required' : 'required' }), label='', max_length=400, required=True)
-    hours = forms.ChoiceField(choices=[(x, x) for x in range(0, 23)],required=True)
-    minutes = forms.ChoiceField(choices=[(x, x) for x in range(0, 59)], required= True)
+    hours = forms.ChoiceField(choices=[(x, x) for x in range(0, 24)],required=True)
+    minutes = forms.ChoiceField(choices=[(x, x) for x in range(0, 60)], required= True)
