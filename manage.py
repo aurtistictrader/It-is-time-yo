01d222e816878d5f9996e0c9ddcecdd87feb8996
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-16 23:21:45 -0500

    set ghetto ping interval

--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
-
+from hello.models.cron import set_interval, timed_job
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
     from django.core.management import execute_from_command_line
 
+    set_interval(timed_job(), 10)
     execute_from_command_line(sys.argv)
