9a5504f2849c95d6aaca1047d4e8eb7027936751
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-16 22:52:05 -0500

    ghetto ping solution

--- hello/cron.py
+++ hello/cron.py
@@ -20,12 +20,9 @@ def timed_job():
 													date_left < (SELECT current_date)''')
 	
 	# Now grab this data and YO it
-    for user,msg in lookup_users:
-    	# do stuff here
+    # for user,msg in lookup_users:
+    # do stuff here
 
 	# Finally delete it
-    Reminder.objects.raw('''DELETE FROM hello_reminder 
-								   WHERE (	time_left <= (SELECT current_time) AND 
-											date_left = (SELECT current_date)) OR 
-											date_left < (SELECT current_date)''')
-    pass    # do your thing here
+    Reminder.objects.raw('''DELETE FROM hello_reminder WHERE (	time_left <= (SELECT current_time) AND date_left = (SELECT current_date)) OR date_left < (SELECT current_date)''')
+    pass
