3a4cf08ae648293f9a634f5ef104998ea5020ee8
Cheng Peng <cwpeng@uwaterloo.ca>
2015-11-27 01:09:33 -0500

    Corrected file variable names

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -63,10 +63,10 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['NAME'],
-        'USER': os.environ['USER'],
-        'PASSWORD': os.environ['PASSWORD'],
-        'HOST': os.environ['HOST'],
+        'NAME': NAME,
+        'USER': USER,
+        'PASSWORD': PASSWORD,
+        'HOST': HOST,
         'PORT': '5432',
     }
 }
