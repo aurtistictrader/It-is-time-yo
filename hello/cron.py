54445d8b16958e54fa406a4d01be6d8f632d2536
Cheng Peng <cwpeng@uwaterloo.ca>
2015-11-27 01:14:10 -0500

    Extracted api-token and fixed conf file

--- hello/cron.py
+++ hello/cron.py
@@ -1,6 +1,7 @@
 from .models import Reminder
 import threading
 from django.db import connection
+from local_settings import API_TOKEN
 import requests
 
 def set_interval(func, sec):
@@ -12,7 +13,7 @@ def set_interval(func, sec):
     return t
 
 def timed_job():
-    api_token = "85f098d7-25c4-404f-baa5-ca0e0d21583c"
+    api_token = API_TOKEN
     # This will be executed every 30 seconds
     # Query into DB and then look for date_left that are past current date
     lookup_users = Reminder.objects.raw(''' SELECT username,id
